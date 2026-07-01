f2af8aa6b69c9718feb6cea3144a00ec043b4f69
mansurarashidrime50 <2230260@iub.edu.bd>
2026-07-01 07:47:18 +0600

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🩸 BloodConnect</h1>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
